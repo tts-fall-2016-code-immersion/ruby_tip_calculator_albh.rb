02b4e964ffe6f1b4bd1a2d9fd399b46ef8e346b4
Brian Hendel <brian.hendel@gmail.com>
2016-09-28 13:46:03 -0400

    Adds variable input and adds output text

--- ruby_tip_calculator_albh.rb
+++ ruby_tip_calculator_albh.rb
@@ -1,18 +1,20 @@
-bill_amount = 100
+#bill_amount = 100
+puts "What is the bill amount?"
+bill_amount = gets.chomp.to_f
 
 def tip_amount(bill_amount)
-  tip = bill_amount * 0.2
+  tip = bill_amount.to_f * 0.2
   return tip
 end
 
-puts tip_amount(bill_amount)
+#puts tip_amount(bill_amount)
 
 def total_with_grat(bill_amount)
-  x = tip_amount(bill_amount)
-  total = x + bill_amount
+  total = tip_amount(bill_amount) + bill_amount
   return total
 end
 
-puts total_with_grat(bill_amount)
+output = total_with_grat(bill_amount).round(2)
+puts "The total including a 20% tip is $#{output}"
 
 #the end
